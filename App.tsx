8540c6aa3693f25f1979b830ad9fb2a6d6628b08
Menard Pajares <menardpajares02@gmail.com>
2023-03-03 19:39:45 +0800

    added QR Register Screen UI

--- App.tsx
+++ App.tsx
@@ -10,12 +10,13 @@ import ChooseRegisterScreen from './src/screens/ChooseRegisterScreen';
 import IdLoginScreen from './src/screens/IdLoginScreen';
 import IdRegisterScreen from './src/screens/IdRegisterScreen';
 import LandingScreen from './src/screens/LandingScreen';
+import QrRegisterScreen from './src/screens/QrRegisterScreen';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <SafeAreaView style={[styles.root, isDarkMode ? styles.rootDark : styles.rootLight]}>
-       <IdRegisterScreen/>
+       <QrRegisterScreen/>
     </SafeAreaView>
   );
 }

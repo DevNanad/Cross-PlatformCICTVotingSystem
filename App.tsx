f296fb842626ef07dcd61174bc37187dec477df7
Menard Pajares <menardpajares02@gmail.com>
2023-03-03 19:13:53 +0800

    added ID Register Screen UI

--- App.tsx
+++ App.tsx
@@ -8,13 +8,14 @@ import {
 import ChooseLoginScreen from './src/screens/ChooseLoginScreen';
 import ChooseRegisterScreen from './src/screens/ChooseRegisterScreen';
 import IdLoginScreen from './src/screens/IdLoginScreen';
+import IdRegisterScreen from './src/screens/IdRegisterScreen';
 import LandingScreen from './src/screens/LandingScreen';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <SafeAreaView style={[styles.root, isDarkMode ? styles.rootDark : styles.rootLight]}>
-       <IdLoginScreen/>
+       <IdRegisterScreen/>
     </SafeAreaView>
   );
 }

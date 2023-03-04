c02bccdeb6d755c5286f791a78fc4988ff855011
Menard Pajares <menardpajares02@gmail.com>
2023-03-04 21:20:22 +0800

    added QR Login Screen UI

--- App.tsx
+++ App.tsx
@@ -10,13 +10,14 @@ import ChooseRegisterScreen from './src/screens/ChooseRegisterScreen';
 import IdLoginScreen from './src/screens/IdLoginScreen';
 import IdRegisterScreen from './src/screens/IdRegisterScreen';
 import LandingScreen from './src/screens/LandingScreen';
+import QrLoginScreen from './src/screens/QrLoginScreen';
 import QrRegisterScreen from './src/screens/QrRegisterScreen';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <SafeAreaView style={[styles.root, isDarkMode ? styles.rootDark : styles.rootLight]}>
-       <QrRegisterScreen/>
+       <QrLoginScreen/>
     </SafeAreaView>
   );
 }
